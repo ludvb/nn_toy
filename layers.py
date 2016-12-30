29418d0323eb1b37e2f4b2b0139b58c9963fe2a2
Ludvig Bergenstråhle <jlbergenstrahle@gmail.com>
2016-12-30 12:52:53 +0100

    Working but needs cleanup

---
+++ layers.py
@@ -0,0 +1,161 @@
+"""Specifies the layer types in the net"""
+
+import numpy as np
+
+# pylint:disable=invalid-name, redefined-builtin
+
+class Layer(object):
+    """Layer class. All layers inherit from this class."""
+    Y, dy = None, None
+    W, dw = None, None
+
+    def __init__(self, X):
+        self.X = X
+        self.dx = np.zeros_like(X)
+        self.fwd()
+
+    def fwd(self):
+        """Abstract method: forward pass"""
+        raise NotImplementedError(
+            "{} not fully implemented".format(self))
+
+    def bck(self):
+        """Abstract method: backward pass"""
+        raise NotImplementedError(
+            "{} not fully implemented".format(self))
+
+    def step(self, lr, reg):
+        """Abstract method: parameter update"""
+        raise NotImplementedError(
+            "{} not fully implemented".format(self))
+
+    def reshape(self, shape):
+        """Reshapes input to the given shape"""
+        pass
+
+
+class input(Layer):
+    """Input layer"""
+
+    def fwd(self):
+        self.Y = self.X
+
+    def bck(self):
+        pass
+
+    def step(self, lr, reg):
+        pass
+
+
+class fc(Layer):
+    """Fully connected layer. N = #(nodes)"""
+
+    def __init__(self, X, N):
+        self.N = N
+        self.i = X.shape[1]  # #(inputs)
+
+        self.dw = None
+        self.reshape(X.shape)
+
+        # Init weights
+        self.W = np.random.uniform(
+            -1, 1, (self.i + 1) * self.N
+        ).reshape(self.i + 1, self.N)
+
+        super().__init__(X)
+
+    def fwd(self):
+        self.Y[...] = np.dot(self._x_bias(), self.W)
+
+    def bck(self):
+        self.dx = np.dot(self.dy, self.W.T[:, :-1])
+        self.dw = np.dot(self._x_bias().T, self.dy)
+
+    def step(self, lr, reg):
+        self.W -= lr * self.dw + 2 * reg * self.W
+
+    def reshape(self, shape):
+        self.n = shape[0]  # batch size
+
+        # Init output
+        self.Y = np.zeros((self.n, self.N))
+
+    def _x_bias(self):
+        """Add bias terms to input"""
+        bias_terms = np.array([1] * self.n)[:, np.newaxis]
+        return np.concatenate((self.X, bias_terms), 1)
+
+
+class relu(Layer):
+    """Rectified linear unit"""
+
+    def fwd(self):
+        self.Y = self.X * (self.X > 0)
+
+    def bck(self):
+        self.dx = self.dy * (self.Y > 0)
+
+    def step(self, lr, reg):
+        pass
+
+
+class sigmoid(Layer):
+    """Sigmoid unit"""
+
+    def fwd(self):
+        self.Y = 1 / (1 + np.exp(-self.X))
+
+    def bck(self):
+        self.dx = self.dy * self.Y * (1 - self.Y)
+
+    def step(self, lr, reg):
+        pass
+
+
+class softmax(Layer):
+    """Softmax layer"""
+
+    def __init__(self, X):
+        self.reshape(X.shape)
+        super().__init__(X)
+
+    def fwd(self):
+        self.Y[...] = np.exp(self.X)
+        self.Y[...] = np.einsum(
+            'ij,i->ij', self.Y, 1 / np.sum(self.Y, 1)
+        )
+
+    def bck(self):
+        dyY = self.dy * self.Y
+        dyYs = np.sum(dyY, 1)
+        self.dx = self.Y * (self.dy.T - dyYs).T
+
+    def step(self, lr, reg):
+        pass
+
+    def reshape(self, shape):
+        self.Y = np.zeros(shape)
+
+
+class loss(Layer):
+    """Loss layer"""
+
+    def __init__(self, X, X_labels):
+        self.Y = np.array([[0.]])
+        self.X_labels = X_labels
+        super().__init__(X)
+
+    def fwd(self):
+        probs = self.X[
+            range(len(self.X[:, 0])),
+            self.X_labels[:, 0]
+        ]
+        self.Y[...] = np.sum(-np.log(probs))
+
+    def bck(self):
+        labels = list(range(len(self.X[0, :])))
+        mlabels = np.array([labels] * len(self.X[:, 0]))
+        self.dx = self.dy * (-1) / (self.X) * (mlabels == self.X_labels)
+
+    def step(self, lr, reg):
+        pass
--- nn.py
+++ nn.py
@@ -1,40 +1,162 @@
 #!/bin/env python3
 
-# pylint: disable=invalid-name
+"""Some module docstring"""
+
+# pylint: disable=invalid-name, no-member
 
 import pdb
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+import layers as L
+
 
 def gen_data(N, K, D):
+    """Generate the data"""
     if N % K != 0:
         raise Exception("N must be divisible by K")
 
     X = np.zeros((N, D))
-    Y = np.zeros((N, 1))
+    Y = np.zeros((N, 1), dtype=int)
 
     for k in range(K):
-        n = N / K
-        r = np.linspace(0, 1, n)
-        t = np.linspace(0, 2 * np.pi / K, n) + 2 * np.pi * k / K
-        t += np.random.uniform(0, 2 * np.pi / 20, n)
+        n = int(N / K)
+        r = np.linspace(0.01, 1, n)
+        t = np.linspace(0, 2 * 2 * np.pi / K, n) + 2 * np.pi * k / K
+        t += np.random.uniform(0, 2 * np.pi / 6, n)
 
-        X[n * k:n * (k + 1), :] = np.array([r * np.cos(t), r * np.sin(t)]).T
-        Y[n * k:n * (k + 1), 0] = k
+        idx = slice(n * k, n * (k + 1))
+        X[idx, :] = np.transpose(
+            np.array([r * np.cos(t), r * np.sin(t)]))
+        Y[idx, 0] = k
 
     return X, Y
 
 
-def main(N=300, K=3, D=2, nodes=100):
+def main(N=300, K=3, D=2, nodes=100, reg=1e-3):
+    """Main"""
+    # Generate and plot data set
     X, Y = gen_data(N, K, D)
-    W = np.uniform(-1, 1, D * nodes).reshape(nodes, D)
 
-    plt.figure()
+    print("Plotting data...")
+    plt.ion()
     plt.subplot(1, 1, 1)
-    plt.scatter(X[:, 0], X[:, 1], c=Y)
-    plt.show()
+    plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=plt.cm.winter)
+    plt.draw()
+
+    input("Press <ENTER> to continue.")
+
+    # Set up layers
+    layers = []
+    layers += [L.input(X)]
+    layers += [L.fc(layers[-1].Y, 50)]
+    layers += [L.sigmoid(layers[-1].Y)]
+    layers += [L.fc(layers[-1].Y, 50)]
+    layers += [L.sigmoid(layers[-1].Y)]
+    layers += [L.fc(layers[-1].Y, 4)]
+    layers += [L.sigmoid(layers[-1].Y)]
+    layers += [L.softmax(layers[-1].Y)]
+    layers += [L.loss(layers[-1].Y, Y)]
+
+    nlayers = len(layers)
+
+    # TODO (architecture): Instead of calling fwd on each layer, connect layers
+    # with "pointers" and call fwd only on the first layer
+
+    try:
+        itx = 1
+        while True:
+            # Forward propagation
+            for i, layer in enumerate(layers):
+                if i == 0:
+                    layer.X = X
+                else:
+                    layer.reshape(layers[i - 1].Y.shape)
+                    layer.X = layers[i - 1].Y
+                layer.fwd()
+
+            if np.isnan(layers[-1].Y[0, 0]):
+                pdb.set_trace()
+
+            print("Iteration {}, Loss = {:.4f}"
+                  .format(itx, np.asscalar(layers[-1].Y)))
+
+            # Backprop
+            for i in list(range(nlayers))[::-1]:
+                if i == nlayers - 1:
+                    layers[i].dy = 1
+                else:
+                    layers[i].dy = layers[i + 1].dx
+
+                layers[i].bck()
+                layers[i].step(1e-3, 1e-8)
+
+            if itx % 5000 == 0:
+                range_ = [np.max(X[:, i]) - np.min(X[:, i]) for i in (0, 1)]
+                x, y = [
+                    np.linspace(np.min(X[:, i]) - range_[i]/2,
+                                np.max(X[:, i]) + range_[i]/2,
+                                400)
+                    for i in (0, 1)
+                ]
+                xx, yy = np.meshgrid(x, y)
+
+                X_ = np.c_[xx.flatten(), yy.flatten()]
+                for i, layer in enumerate(layers[:-1]):
+                    if i == 0:
+                        layer.X = X_
+                    else:
+                        layer.reshape(layers[i - 1].Y.shape)
+                        layer.X = layers[i - 1].Y
+                    layer.fwd()
+                z = np.argmax(layers[-2].Y, axis=1).reshape(xx.shape)
+
+                plt.clf()
+                plt.contourf(xx, yy, z, cmap=plt.cm.winter)
+                plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=plt.cm.winter)
+                plt.draw()
+                plt.pause(1e-10)
+
+            if False:  # Gradient check
+                for i in list(range(nlayers))[::-1]:
+                    if np.all(layers[i].dx == 0):
+                        continue
+
+                    print("Checking gradient on {}".format(layers[i]))
+                    r, c = [
+                        np.random.choice(layers[i].X.shape[j])
+                        for j in (0, 1)
+                    ]
+                    h = 1e-8
+
+                    Y_ = []
+                    for X_ in [
+                            layers[i].X[r, c] + s * h
+                            for s in (-1, 1)
+                    ]:
+                        layers[i].X[r, c] = X_
+
+                        for j in range(i, nlayers):
+                            if j > i:
+                                layers[j].reshape(layers[j - 1].Y.shape)
+                                layers[j].X = layers[j - 1].Y
+                            layers[j].fwd()
+
+                        Y_.append(np.asscalar(layers[-1].Y))
+
+                    ndx = (Y_[1] - Y_[0]) / (2 * h)
+                    print("Analytical: {}, Numerical: {}".format(
+                        layers[i].dx[r, c], ndx
+                    ))
+                    input("")
+
+            itx += 1
+
+    except KeyboardInterrupt:
+        # print(layers[-2].Y)
+        pass
+
 
 if __name__ == "__main__":
-    main()
+    main(300, 4, 2, 5)
